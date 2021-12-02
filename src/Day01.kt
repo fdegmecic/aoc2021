e6166f90976bc7b8dcd4a11cc5d2505e5511e846
Filip Degmecic <filip_degmecic@yahoo.com>
2021-12-02 20:09:15 +0100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,18 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Int {
+        return input.zipWithNext().count { (a, b) -> b > a }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Int>): Int {
+        return input.windowed(3).zipWithNext().count { (a, b) ->
+            a.sum() < b.sum()
+        }
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInputToInts("Day01_test")
+    check(part2(testInput) == 5)
 
-    val input = readInput("Day01")
+    val input = readInputToInts("Day01")
     println(part1(input))
     println(part2(input))
 }
