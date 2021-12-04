0e6efa812603a22c9b7d150509fe7b0779cde462
Filip Degmecic <filip_degmecic@yahoo.com>
2021-12-04 12:24:04 +0100

    Day02

---
+++ src/Day02.kt
@@ -0,0 +1,91 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        val submarine = Submarine()
+
+        val operations = transformInput(input)
+
+        operations.map { (direction, amount) ->
+            when (direction) {
+                "forward" -> {
+                    submarine.forward(amount)
+                }
+                "up" -> {
+                    submarine.up(amount)
+                }
+                "down" -> {
+                    submarine.down(amount)
+                }
+            }
+        }
+
+        return submarine.depth * submarine.horizontalPosition
+    }
+
+    fun part2(input: List<String>): Int {
+        val submarine = Submarine()
+
+        val operations = transformInput(input)
+
+        operations.map { (direction, amount) ->
+            when (direction) {
+                "forward" -> {
+                    submarine.aimForward(amount)
+                }
+                "up" -> {
+                    submarine.aimUp(amount)
+                }
+                "down" -> {
+                    submarine.aimDown(amount)
+                }
+            }
+        }
+
+        return submarine.depth * submarine.horizontalPosition
+    }
+
+    val testInput = readInput("Day02_test")
+    check(part2(testInput) == 900)
+
+    val input = readInput("Day02")
+    println(part1(input))
+    println(part2(input))
+}
+
+class Submarine(
+    var depth: Int = 0,
+    var horizontalPosition: Int = 0,
+    var aim: Int = 0,
+) {
+    fun forward(amount: Int) {
+        horizontalPosition += amount
+    }
+
+    fun up(amount: Int) {
+        depth -= amount
+    }
+
+    fun down(amount: Int) {
+        depth += amount
+    }
+
+    fun aimForward(amount: Int) {
+        horizontalPosition += amount
+        depth += aim * amount
+    }
+
+    fun aimUp(amount: Int) {
+        aim -= amount
+    }
+
+    fun aimDown(amount: Int) {
+        aim += amount
+    }
+}
+
+data class Operations(
+    val direction: String,
+    val amount: Int,
+)
+
+fun transformInput(input: List<String>): List<Operations> =
+    input.flatMap { it.split(" ").zipWithNext { a, b -> Operations(a, b.toInt()) } }
